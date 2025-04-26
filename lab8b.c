b13bf972b4bb488ba7a3ab68515208c9c4765ad9
ansira-github <fathimath.23cs038@sode-edu.in>
2025-04-26 15:17:27 +0530

    a.Floyds & b.Warshals _algorithm

---
+++ lab8b.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define MAX 20
+void warshall(int n,int r[MAX][MAX]){
+int i,j,k;
+for(k=1;k<=n;k++){
+for(i=1;i<=n;i++){
+for(j=1;j<=n;j++){
+r[i][j]=r[i][j]||(r[i][k]&&r[k][j]);
+}}}
+
+printf("transitive closure matrux is:\n");
+for(i=1;i<=n;i++){
+printf("\t%d",i);
+}
+printf("\n\t-----------------------\n");
+for(i=1;i<=n;i++){
+printf("%d|\t",i);
+for(j=1;j<=n;j++){
+printf("%d\t",r[i][j]);
+}
+printf("\n");
+}}
+
+int main(){
+int r[MAX][MAX];
+
+int i,j,n;
+printf("enter the number of nodes/verties:");
+scanf("%d",&n);
+printf("enter the adjacent matrix\n");
+
+for(i=1;i<=n;i++){
+for(j=1;j<=n;j++){
+scanf("%d",&r[i][j]);
+}}
+warshall(n,r);
+return 0;
+}
